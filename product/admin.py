8df712f9096c5b396265bc6343aab9c9c999e48d
unknown <muzhirulislam@gmail.com>
2023-07-06 11:11:48 +0600

    add category table in model and categoryadmin in admin

--- product/admin.py
+++ product/admin.py
@@ -4,12 +4,23 @@ from product.models import *
 # Register your models here.
 class BrandAdmin(admin.ModelAdmin):
     fieldsets = [
-        ("Basic Info",{"fields":['name','slug','status']}),
-        ("Description",{"fields":["description"]}),
+        ("Basic Info",{"fields":[('name','slug','status')]}),
+        ("Description",{"fields":[("description")]}),
     ]
     list_display = ['name','slug','created_by','created_at','status']
     prepopulated_fields = {'slug':('name',)}
     search_fields = ['name','slug','created_by']
+    list_filter = ['name']
 
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Basic Info",{"fields":[('parent'),('name','slug','status')]}),
+        ("Description",{"fields":[("description")]}),
+    ]
+    list_display = ['name','slug','created_by','created_at','status']
+    prepopulated_fields = {'slug':('name',)}
+    search_fields = ['name','slug','created_by']
+    list_filter = ['name']
 
 admin.site.register(Brand,BrandAdmin)
+admin.site.register(Category,CategoryAdmin)
--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django_userforeignkey.models.fields import UserForeignKey
 
 # Create your models here.
 class Brand(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Brand Name")
+    name = models.CharField(max_length=255, verbose_name="Brand Name",db_index=True)
     slug = models.SlugField(max_length=255,verbose_name="Brand Slug")
     description = models.TextField(null=True)
     status = models.BooleanField(default=True)
@@ -14,7 +14,26 @@ class Brand(models.Model):
 
     class Meta:
         db_table = 'brand'
-        verbose_name = 'Brand'
+        verbose_name = '1. Brand'
+    
+    def __str__(self):
+        return self.name
+    
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE,null=True, related_name='sub_category')
+    name = models.CharField(max_length=255, verbose_name="Category Name",db_index=True)
+    slug = models.SlugField(max_length=255,verbose_name="Category Slug")
+    description = models.TextField(null=True)
+    status = models.BooleanField(default=True)
+    created_by = UserForeignKey(auto_user_add=True, on_delete=models.SET_NULL,related_name='category_creator', editable=False, blank=True, null=True)
+    last_modified_by = UserForeignKey(auto_user=True, on_delete=models.SET_NULL, related_name='category_updated_by', editable=False, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = '2. Category'
     
     def __str__(self):
         return self.name
